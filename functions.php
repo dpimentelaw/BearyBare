0b02731c489c4eeab14257b2650f1b3c099e882c
dpimentelaw <dpimentel@alphaworks.tech>
2022-04-27 16:00:15 -0700

    fixed functions

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function theme_files()
 {
 
     wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'bootstrap', 'gihttps://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
 
     wp_enqueue_script( 'bootstrap.js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array('jquery'), true, true);
     wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom.js', array('jquery'), true, true );
@@ -21,7 +21,7 @@ function bearyBare_features() {
 add_action('after_setup_theme','bearyBare_features');
 
 function staff_post_types() { 
-    register_post_type('ceo',array (//This needs to be changed
+    register_post_type('ceo',array (
       'supports' => array('title','editor','thumbnail','custom-fields'),
       'public' =>true,
       'show_in_rest' =>true,
